186445435177eb07b166858f18add2594953d0d6
Eric Lluis <ericlluis84@gmail.com>
2024-03-28 09:38:08 +0100

    Assign the variable areSame a Boolean value.

---
+++ src/songs.ts
@@ -0,0 +1,57 @@
+import { Song, existingSongs } from "./songsData";
+
+export const areSameSong = (song: Song, songTitle: string): boolean => {
+  return song.title === songTitle;
+};
+
+export const addSong = (song: Song, songs: Song[]): void => {
+  let title = songs.push(song);
+};
+
+export const isPlaylistFull = (songs: Song[]): boolean => {
+  return songs.length >= 4;
+};
+
+export const getErrorMessage = (errorCode: string): string => {
+  switch (errorCode) {
+    case "exists":
+      return "La canción ya existe";
+    case "limit":
+      return "La playlist está llena";
+    default:
+      return "Código de error desconocido";
+  }
+};
+
+export const getSongsCount = (songs: Song[]): number => {
+  return songs.length;
+};
+
+export const removeSongByPosition = (songs: Song[], position: number): void => {
+  if (position < 0) {
+    position = 0;
+  }
+  songs.splice(position, 1);
+};
+
+export const setCurrentSong = (song: Song): void => {
+  song.isCurrent = true;
+};
+
+export const getNextSongPosition = (
+  songs: Song[],
+  currentSongPosition: number
+) => {
+  let nextSongPosition: number;
+  nextSongPosition = currentSongPosition + 1;
+  if (nextSongPosition >= songs.length) {
+    nextSongPosition = 0;
+  }
+
+  return nextSongPosition;
+};
+
+export const isCurrentSong = (song: Song): boolean => {
+  let isCurrent: boolean;
+  return song.isCurrent;
+};
